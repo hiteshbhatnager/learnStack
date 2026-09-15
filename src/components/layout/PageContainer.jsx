df88c99f4435922b2d3d6be5c8510ac9498baaa8
hitesh <bhatnagerhitesh@gmail.com>
2026-09-15 22:22:46 +0530

    re consider

--- src/components/layout/PageContainer.jsx
+++
@@ -1,9 +0,0 @@
-import './PageContainer.css';
-
-export default function PageContainer({ children, className = '' }) {
-  return (
-    <main className={`page-container ${className}`}>
-      {children}
-    </main>
-  );
-}
